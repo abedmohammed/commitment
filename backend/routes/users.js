e01ef0a7d9a78d35f062799fdd2c3700f4c729fc
jinalkast <jinalkast@gmail.com>
2024-01-13 21:19:34 -0500

    created backend for the application

---
+++ backend/routes/users.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+const UserData = require('../models/data')
+
+// get all
+router.get('/', async (req, res) => {
+    try {
+        const users = await UserData.find()
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// get one
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+// create one
+router.post('/', async (req, res) => {
+    const user = new UserData({
+        habits: req.body.habits
+    })
+
+    try {
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+// update one
+router.patch ('/', (req, res) => {
+
+})
+
+// delete one 
+router.delete('/', (req, res) => {
+
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/subscribers')
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
-db.once('open', ()=> console.log('Connection to Database'))
+db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
-const subscribersRouter  = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+const usersRouter  = require('./routes/users')
+app.use('/users', usersRouter)
 
 port = 5000
 app.listen(port, () => {console.log(`Server started on port ${port}`)})
