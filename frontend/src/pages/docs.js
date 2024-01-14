cd21d9ba3362a2e8a30d730c90d208abed6548ce
jinalkast <jinalkast@gmail.com>
2024-01-14 10:32:29 -0500

    updated api docs

--- frontend/src/pages/docs.js
+++ frontend/src/pages/docs.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import React from "react";
 
 import keysImage from "../../public/images/keys.png";
+import graphImage from "../../public/images/graph.png";
+import timerImage from "../../public/images/timer.png";
 import { CopyBlock, a11yDark } from "react-code-blocks";
 
 const docs = () => {
@@ -18,6 +20,12 @@ const docs = () => {
       }),
       }).then((res) => res.json());`;
 
+  const getAll = `fetch("http://localhost:5000/habits")
+  .then((res) => res.json());`
+
+  const getOne = `fetch("http://localhost:5000/habits/ID")
+  .then((res) => res.json());`
+
   return (
     <>
       <Header />
@@ -32,7 +40,8 @@ const docs = () => {
         <h2 className="docs__subtitle">Habit ID</h2>
         <p className="docs__paragraph">
           To begin creating automation for your habits, find the corresponding
-          habit id from your dashboard settings
+          habit id from your dashboard settings. This value will be used in all API calls
+          to access the desired habit for reading/writing.
         </p>
         <Image
           className="docs__image"
@@ -40,14 +49,50 @@ const docs = () => {
           width={700}
           quality={100}
         />
-
         <h2 className="docs__subtitle">Get Data</h2>
-
+        <p className = "docs__paragraph">
+          The public GET calls provided by our API will allow you to take your associated habit data
+          and tranform the user interface into anything that will help you visualize the data in the
+          way that is most beneficial to you. For example, if a github commit chart isn't your own style,
+          and you'd like to take on the challenge of building your own application, you could try transforming
+          the habits to a graph, as with the <i>Weekly Runs</i> habit in the figure below: </p>
+        <Image
+          className="docs__image"
+          src={graphImage}
+          width={700}
+          quality={100}
+        />
+        <p className = "docs__paragraph">
+          There are 2 GET calls that can be used to return whichever data you deem necessary for your project.
+          The first call can be used to get a single instance of a habit object, identified by its unique ID:
+        </p>
+        <div className="docs__code">
+          <CopyBlock
+            text={getOne}
+            language={"javascript"}
+            theme={a11yDark}
+            showLineNumbers={true}
+            wrapLines
+          />
+        </div>
+        <p className = "docs__paragraph">
+          The second call returns all of the habit objects, contained in a single array:
+        </p>
+        <div className="docs__code">
+          <CopyBlock
+            text={getAll}
+            language={"javascript"}
+            theme={a11yDark}
+            showLineNumbers={true}
+            wrapLines
+          />
+        </div>
         <h2 className="docs__subtitle">Submit Entry</h2>
         <p className="docs__paragraph">
           You can use the public PATCH call to submit your own habit commits
           easily. This can be done by sending a PATCH request of your choice
-          with the correct parameters, and using the corresponding habit id
+          with the correct parameters, and using the corresponding habit id.
+          Sample javascript code is shown below:
         </p>
         <div className="docs__code">
           <CopyBlock
@@ -58,6 +103,21 @@ const docs = () => {
             wrapLines
           />
         </div>
+        <p className="docs__paragraph">
+          The sky is really the limit with this call, it can be used to integrate commitment's APIs and
+          data stores to any project that you would like. For example, you could create a simple Pomodoro timer
+          to commit your focus time directly to the commitment app with just a click:
+        </p>
+        <Image
+          className="docs__image"
+          src={timerImage}
+          width={700}
+          quality={100}
+        />
+        <p className="docs__paragraph">
+          That's all for now! We hope to add support for more use of your data in the future, and we hope
+          you have as much fun implementing your own projects with our APIs, as we did creating commitment!
+        </p>
       </main>
     </>
   );
